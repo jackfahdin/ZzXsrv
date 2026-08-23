a500e2eb0891f4525bdaa5df7fc263d3f75bc91e
Jackfahdin.Guo <guomaojie@pudutech.com>
2026-08-23 22:36:50 +0800

    ci: 新增 GitHub Actions Windows 构建链
    - 复刻上游手工构建链到 windows-2022 runner：VS2022(vswhere 探测)
      + Strawberry Perl + NASM + winflexbison + jom + Cygwin 工具包
      + Python3.9(lxml/mako) + WSL1 承载 buildall.sh
    - setenv.bat 去除 VS Community 路径硬编码，改 vswhere 定位
      （runner 预装 Enterprise，原硬编码路径不存在）

--- setenv.bat
+++ setenv.bat
@@ -2,9 +2,8 @@
 set PATH=%PATH%;c:\windows\system32\Wbem
 set > env_before.txt
 
-pushd "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
-CALL vcvarsall.bat %1 > nul
-popd
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do set "VSINSTALL=%%i"
+call "%VSINSTALL%\VC\Auxiliary\Build\vcvarsall.bat" %1 > nul
 
 set > env_after.txt
 
